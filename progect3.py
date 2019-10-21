7b1d060068a9a89de17425523ea87724a955ea0a
Grinvalera <56276566+Grinvalera@users.noreply.github.com>
2019-10-22 00:13:48 +0300

    update 2.0

--- progect3.py
+++ progect3.py
@@ -51,35 +51,91 @@ def task3(line):
      return line
 
 
-'''def task47(line = "hello abc3, how are abc4 you? abc please return in our world abc6"):
-    b = 'abc'
-    for char in b:
-        a = line.replace(char, "")
-        print(a)'''
+def task47(line):
+    a = len(line)
+    for i in range(1):
+        for _ in range(1):
+            line = line.replace('abc', '')
+            line = line.replace('1234567890', '')
+            print(line)
+        else:
+            return line
+
+
+from re import *
+def task22(address):
+    pattern = compile('(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)')
+    is_valid = pattern.match(address)
+    if is_valid:
+        print('правильный email:', is_valid.group())
+    else:
+        print('неверный email!')
+
+
+def task29(line):
+    a = line.replace('a', 'ab')
+    print(a)
+    return a
+
+
+def task60(line1, line2):
+    line3 = line1.replace(line2, '')
+    print(line3)
+    return line3
+
+
+def task42(line1):
+    a = line1.index(',') + 1
+    b = line1.index(',', a)
+    c = line1[a: b:]
+    print(c)
+    return line1
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#task47()
-
+print('task3')
 task3('Hello oooo iknfo')
 
-task34('sdjgjcvblksjbhvgiauhgrhjgjk')
+print('task16')
+task16('JavaScript', 'Pascal')
 
-task8('w12345x')
+print('task29')
+task29('aaaaaaaaaaaaaaaaaaaaa')
 
+print('task42')
+task42('Hello, people, word')
+
+print('task55')
 task55('a1b2c3d4')
 
-task16('JavaScript', 'Pascal')
+print('task8')
+task8('w12345x')
+
+print('task22')
+task22('valera.grinevich@gmail.com')
+
+print('task34')
+task34('sdjgjcvblksjbhvgiauhgrhjgjk')
+
+print('task47')
+task47('hello abc3, how are abc4 you? abc2 please return in our world abc6')
+
+print('task60')
+task60('Hello word, the weather is good', 'weather' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
